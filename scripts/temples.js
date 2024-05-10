a74268a34b67cc776cca53136c67bad6399b7798
PieroArbildo <89119204+PieroArbildo@users.noreply.github.com>
2024-05-10 07:21:09 -0500

    Picture Album added

---
+++ scripts/temples.js
@@ -0,0 +1,39 @@
+const mainnav = document.querySelector(".navigation");
+const hambutton = document.querySelector("#menu");
+const title = document.querySelector("#title");
+
+const headerDiv = document.querySelector("header div");
+const dinamicHeading = document.querySelector("main h2");
+const liA = document.querySelectorAll("header nav ul li a");
+
+hambutton.addEventListener("click", () => {
+  mainnav.classList.toggle("show");
+  hambutton.classList.toggle("show");
+
+  if (title.style.display === "none") {
+    title.style.display = "block";
+    hambutton.style.position = "";
+    hambutton.style.right = "0";
+    hambutton.style.marginTop = "0px";
+    headerDiv.style.height = "80px";
+  } else {
+    title.style.display = "none";
+    hambutton.style.position = "fixed";
+    hambutton.style.right = "3px";
+    hambutton.style.marginTop = "50px";
+    headerDiv.style.height = "20px";
+  }
+});
+
+liA.forEach(function (liA) {
+  liA.addEventListener("click", function () {
+    dinamicHeading.innerHTML = liA.innerHTML;
+  });
+});
+
+const today = new Date();
+const year = document.querySelector("#currentyear");
+year.innerHTML = `<span
+  class="highlight">${today.getFullYear()}</span>`;
+const lastModified = document.querySelector("#lastmodification");
+lastModified.innerHTML = `Last modification: <span class="highlight">${document.lastModified} (GMT-5)</span>`;
