be7572166f7cf80884521d4496e81d4842f6e8fb
PieroArbildo <89119204+PieroArbildo@users.noreply.github.com>
2024-06-04 03:56:43 -0500

    fixed errors

--- project/scripts/gastronomic.js
+++ project/scripts/gastronomic.js
@@ -16,50 +16,50 @@ const gastronomic = [
       mealName: "Pollo a la Brasa",
       ingredients: "Whole chicken marinated in a mixture of spices and herbs, then roasted on a grill or rotisserie.",
       imageUrl:
-        "https://github.com/PieroArbildo/wdd131/tree/main/project/images/pollo-brasa.webp",
+        "https://raw.githubusercontent.com/PieroArbildo/wdd131/main/project/images/pollo-brasa.webp",
     },
     {
         mealName: "Papa a la huancaina",
       ingredients: "Boiled potatoes topped with a creamy sauce made with yellow chili pepper, fresh cheese, milk, crackers, and other ingredients, typically served with hard-boiled eggs and olives.",
       imageUrl:
-        "https://github.com/PieroArbildo/wdd131/tree/main/project/images/papa.webp",
+        "https://raw.githubusercontent.com/PieroArbildo/wdd131/main/project/images/papa.webp",
     },
     {
       mealName: "Rocoto Relleno",
       ingredients: "Rocoto peppers stuffed with a mixture of ground meat, onions, garlic, cheese, and other ingredients, baked and often served with potatoes and melted cheese.",
       imageUrl:
-        "https://github.com/PieroArbildo/wdd131/tree/main/project/images/rocoto.jpg",
+        "https://raw.githubusercontent.com/PieroArbildo/wdd131/main/project/images/rocoto.jpg",
     },
     {
       mealName: "Arroz con Pollo",
       ingredients: "Chicken cooked with rice, seasoned with spices such as turmeric, cumin, and garlic, typically cooked with vegetables such as onions, bell peppers, and carrots.",
       imageUrl:
-        "https://github.com/PieroArbildo/wdd131/tree/main/project/images/images/arroz-pollo.webp",
+        "https://raw.githubusercontent.com/PieroArbildo/wdd131/main/project/images/arroz-pollo.webp",
     },
     {
       mealName: "Suspiro a la Limeña",
       ingredients: "Evaporated milk, condensed milk, sugar, egg yolks, vanilla essence, and Italian meringue.",
       imageUrl:
-        "https://github.com/PieroArbildo/wdd131/tree/main/project/images/images/suspiro.jpg",
+        "https://raw.githubusercontent.com/PieroArbildo/wdd131/main/project/images/suspiro.jpg",
     },
     
     {
       mealName: "Arroz con Leche",
       ingredients: "Rice, milk, sugar, cinnamon stick, and lemon peel.",
       imageUrl:
-        "https://github.com/PieroArbildo/wdd131/tree/main/project/images/images/arroz-leche.webp",
+        "https://raw.githubusercontent.com/PieroArbildo/wdd131/main/project/images/arroz-leche.webp",
     },
     {
       mealName: "Mazamorra Morada",
       ingredients: "Purple corn, pineapple, quince, cinnamon, cloves, sugar, and sweet potato.",
       imageUrl:
-        "https://github.com/PieroArbildo/wdd131/tree/main/project/images/images/mazamorra.webp",
+        "https://raw.githubusercontent.com/PieroArbildo/wdd131/main/project/images/mazamorra.webp",
     },
     {
       mealName: "Picarones",
       ingredients: "Pumpkin, wheat flour, sugar, anise, yeast, and chancaca (sugar syrup).",
       imageUrl:
-        "https://github.com/PieroArbildo/wdd131/tree/main/project/images/images/picaron.webp",
+        "https://raw.githubusercontent.com/PieroArbildo/wdd131/main/project/images/picaron.webp",
     },
   ];
   
